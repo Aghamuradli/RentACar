149436b906bb3abcc28399e547f52fc6862d8014
BabekAga <82724876+BabekAga@users.noreply.github.com>
2022-10-21 00:09:17 +0400

    Layers created, car entity, service, manager added

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+            new Car{Id=1,BrandId=2,ColorId=10,DailyPrice=4100,ModelYear=2010,Description="BMW10" },
+            new Car{Id=2,BrandId=7,ColorId=12,DailyPrice=5100,ModelYear=2012,Description="BMW12" },
+            new Car{Id=3,BrandId=8,ColorId=13,DailyPrice=2300,ModelYear=2008,Description="BMW08" },
+            new Car{Id=4,BrandId=10,ColorId=12,DailyPrice=4400,ModelYear=2011,Description="BMW11" }
+
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            return _cars.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.Id= car.Id;
+            carToUpdate.BrandId=car.BrandId;
+            carToUpdate.ColorId=car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
