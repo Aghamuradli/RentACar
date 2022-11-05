b929bb259088acfd6ea69095bb2cbd72adb18ed8
BabekAga <82724876+BabekAga@users.noreply.github.com>
2022-11-05 21:23:47 +0400

    Interceptors, AOP, Validation structure added

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -20,18 +23,13 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
-
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.Brand.Name.Length < 2 || car.DailyPrice <= 0)
-            {
-                return new ErrorResult(Messages.CarNameInvalid);
-            }
-            else
-            {
+            
                 _carDal.Add(car);
                 return new SuccessResult(Messages.CarAdded);
-            }
+
         }
         public IResult Delete(Car car)
         {
